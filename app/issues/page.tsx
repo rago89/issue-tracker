2312ad08be09b285ee23517b80049c54379eb8e8
Rafael Garcia <rafaelandres.garciaortega@fedasil.be>
2025-01-24 10:39:51 +0100

    set up radixUI

---
+++ app/issues/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@radix-ui/themes";
+
+const page = () => {
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
+};
+
+export default page;
