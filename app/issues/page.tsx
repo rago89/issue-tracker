5eeb29dffd381e6a9128a5265871390b154ea2c2
Rafael Garcia <rafaelandres.garciaortega@fedasil.be>
2025-01-24 11:56:57 +0100

    build new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
-const page = () => {
+const Issue = () => {
   return (
     <div>
-      <Button>New Issue</Button>
+      <Button>
+        <Link href="/issues/new">New Issue</Link>
+      </Button>
     </div>
   );
 };
 
-export default page;
+export default Issue;
