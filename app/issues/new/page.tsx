9d8d3749be2d47375dab777a4c8833940dadef51
Rafael Garcia <rafaelandres.garciaortega@fedasil.be>
2025-01-24 15:45:15 +0100

    add error handling post issue

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import React from "react";
+import React, { use, useEffect } from "react";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import { useForm, Controller } from "react-hook-form";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useRouter } from "next/navigation";
 
 interface NewIssueForm {
@@ -15,25 +15,59 @@ interface NewIssueForm {
 const NewIssuePage = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<NewIssueForm>();
+  const [error, setError] = React.useState<string | null>(null);
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        setError(null);
+      }, 2000);
+    }
+  }, [error]);
+
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async data => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}>
-      <TextField.Root placeholder="Title" {...register("title")}>
-        <TextField.Slot></TextField.Slot>
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
-      <Button>Submit New Issue</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className=" space-y-3"
+        onSubmit={handleSubmit(async data => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error: unknown) {
+            if (axios.isAxiosError(error)) {
+              const axiosError = error as AxiosError;
+              let errorMessage = "";
+              if (axiosError.response?.status === 400) {
+                (
+                  axiosError.response.data as { error: z.ZodError[] }
+                ).error.forEach(err => {
+                  errorMessage += " - " + err.message;
+                });
+                if (errorMessage) {
+                  setError(errorMessage);
+                }
+              }
+            }
+          }
+        })}>
+        <TextField.Root placeholder="Title" {...register("title")}>
+          <TextField.Slot></TextField.Slot>
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
