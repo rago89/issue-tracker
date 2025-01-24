3ac0e15e22caa486f7e532a58d661203ccf10749
Rafael Garcia <rafaelandres.garciaortega@fedasil.be>
2025-01-24 16:29:33 +0100

    remove import

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Navbar";
-import { Theme, ThemePanel } from "@radix-ui/themes";
+import { Theme } from "@radix-ui/themes";
 
 const geistSans = Geist({
   subsets: ["latin"],
