3578bb0c746b464e2326766962cf96bef2983a2b
Rafael Garcia <rafaelandres.garciaortega@fedasil.be>
2025-01-27 12:11:59 +0100

    adding additional issue skeleton

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Box, Card, Flex } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const loadingSingleIssue = () => {
-  return <div>loadingSingleIssue</div>;
+  return (
+    <Box className="max-w-xl">
+      <Skeleton width="5rem" />
+      <Flex gap="5" className="my-5">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose" mt="4">
+        <Skeleton count={5} />
+      </Card>
+    </Box>
+  );
 };
 
 export default loadingSingleIssue;
