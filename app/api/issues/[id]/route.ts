620efe3fd5d83afaa7d0eac722aab6eabfb253ca
Rafael Garcia <rafaelandres.garciaortega@fedasil.be>
2025-01-28 10:29:01 +0100

    build an api for updating issues

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,41 @@
+import { createIssueSchema } from "@/app/validationSchemas/createIssueSchema";
+import { prisma } from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const id = Number(params.id);
+  if (isNaN(id)) {
+    return NextResponse.json({ error: "Invalid id" }, { status: 400 });
+  }
+  const validation = createIssueSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(
+      { error: validation.error.format() },
+      { status: 400 }
+    );
+  }
+
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!issue) {
+    return NextResponse.json({ error: "Issue not found" }, { status: 404 });
+  }
+
+  const updatedIssue = await prisma.issue.update({
+    where: {
+      id: id,
+    },
+    data: validation.data,
+  });
+
+  return NextResponse.json(updatedIssue);
+}
